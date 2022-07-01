c3b8aa23728eb3f77d132bef75d2517e19292203
Sayan Roy <sayan98.roy@gmail.com>
2022-07-01 23:51:45 +0530

    01-Jully-2022

---
+++ pattern/pattern7.cpp
@@ -0,0 +1,30 @@
+// Enter5
+// * 
+// * *
+// * * *
+// * * * *
+// * * * * *
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cout<<"Enter";
+    cin>>n;
+    int row = 1;
+    while ( row <= n)   
+    {
+        int col = 1;
+        while ( col <= row)
+        {
+            cout<<"* ";
+            col++;
+        }
+        cout<<endl;
+        row++;
+        
+    }
+    
+    return 0;
+}
